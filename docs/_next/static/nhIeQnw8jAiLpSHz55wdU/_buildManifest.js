0793bece08eb037a07e7876e44e8d444face8902
tayydev <spam@tayy.dev>
2023-08-03 20:26:42 -0400

    Initial Release of new Portfolio

---
+++ docs/_next/static/nhIeQnw8jAiLpSHz55wdU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/825-1940c88e8adf48e4.js","static/css/d0cef05d77cbcdcc.css","static/chunks/pages/index-c38122d515f686a6.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
