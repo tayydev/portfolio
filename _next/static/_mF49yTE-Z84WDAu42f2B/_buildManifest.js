90886d9400a9dcce03ec22b736016223b2d4dc36
tayydev <51377975+tayydev@users.noreply.github.com>
2024-04-24 19:20:46 +0000

    Deploying to gh-pages from @ tayydev/portfolio@dff56cdd964b7c5d70bbf15fbd97a43e4cc37999 🚀

---
+++ _next/static/_mF49yTE-Z84WDAu42f2B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/971-2b6e9d0c698c8b84.js","static/css/b523295ea750b4c0.css","static/chunks/pages/index-9c86e36ec030f042.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
