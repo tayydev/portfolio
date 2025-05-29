7ea0feb82e106766278663f72ebab61082538166
tayydev <51377975+tayydev@users.noreply.github.com>
2025-05-28 20:08:37 -0400

    Beautiful

---
+++ src/lib/Pallete.svelte
@@ -0,0 +1,23 @@
+<main>
+    <div class="reset">
+        <div class="square" style="background: var(--color-pale-tan)"></div>
+        <div class="square" style="background: var(--color-pale-green)"></div>
+        <div class="square" style="background: var(--color-link-green)"></div>
+    </div>
+</main>
+
+<style>
+    .reset {
+        background: black;
+        width: fit-content;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 0.25rem;
+        padding: 0.25rem;
+    }
+    .square {
+        width: 2rem;
+        height: 2rem;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+    import Pallete from "$lib/Pallete.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Pallete/>
