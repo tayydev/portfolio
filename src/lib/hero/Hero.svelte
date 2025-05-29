4edc4f2cc6ecd711d4c2173cd56d531b72e2c15b
tayydev <51377975+tayydev@users.noreply.github.com>
2025-05-28 22:28:10 -0400

    Hot

--- src/lib/hero/Hero.svelte
+++ src/lib/hero/Hero.svelte
@@ -1,6 +1,6 @@
 <script>
     import Palette from "$lib/Pallete.svelte";
-    import hero from "./hero_reduced.webp"
+    import hero from "./hero_reduced_3x2.webp"
     import Center from "$lib/Center.svelte";
     import HeroText from "$lib/hero/HeroText.svelte";
     import HeroPolaroid from "$lib/hero/HeroPolaroid.svelte";
@@ -9,7 +9,8 @@
 <main>
     <Center>
         <div class="hero-container">
-            <HeroText>TAYLOR<br>SOUTH</HeroText>
+            <HeroText style="--image-start: 200px">TAYLOR</HeroText>
+            <HeroText><br>SOUTH</HeroText>
             <HeroPolaroid/>
             <div class="palette-wrapper">
                 <img class="hero-img" src={hero} alt="taylor at a beach"/>
